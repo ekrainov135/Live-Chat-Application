54d20f771c1be3ededc5da298d7d5af0fd223b52
ekrainov135 <ekrainov135@gmail.com>
2020-09-22 13:17:23 +0300

    refactoring; added handling and generation of exceptions; adjusted connection closing logic

--- core/server.py
+++ core/server.py
@@ -1,6 +1,6 @@
 
-import asyncio
 import json
+import time
 from abc import ABC, abstractmethod
 
 from core.services import ChatStorageDriver
@@ -10,23 +10,14 @@ class BaseServerManager(ABC):
     """ Base class of a low-level tcp-server """
 
     async def read(self, reader):
-        data_text = await reader.read(1024)
-        return json.loads(data_text.decode())
+        data = b''
+
+        while not data.endswith(b'\n'):
+            data += await reader.read(1024)
+        return json.loads(data.decode())
 
     def write(self, writer, data_json):
-        response = json.dumps(data_json)
-        writer.write(response.encode()+b'\n')
-
-    def start(self, host='127.0.0.1', port=7070):
-        self._loop = asyncio.get_event_loop()
-        self._coro = asyncio.start_server(self.connection_handle, host, int(port), loop=self._loop)
-        self.server = self._loop.run_until_complete(self._coro)
-        self._loop.run_forever()
-
-    def close(self):
-        self.server.close()
-        self._loop.run_until_complete(self.server.wait_closed())
-        self._loop.close()
+        writer.write(json.dumps(data_json).encode()+b'\n')
 
     @abstractmethod
     async def connection_handle(self, reader, writer):
@@ -39,16 +30,18 @@ class ChatServerManager(BaseServerManager):
         """ Decorator for functions using login.  """
 
         async def wrapped(self, writer, data_json):
-            if writer.get_extra_info('peername') not in self.members:
-                return
-            return await func(self, writer, data_json)
+            peername = writer.get_extra_info('peername')
+            is_identified = peername in self.members
+            is_authenticated = is_identified and self.members[peername].username == data_json['username']
+
+            return await func(self, writer, data_json) if is_identified and is_authenticated else None
         return wrapped
 
     def __init__(self):
         self.storage = ChatStorageDriver('storage.json')
 
         # Protocol method handlers
-        self.handlers = {'login': self.connection_login, 'logout': self.connection_logout, 'send': self.connection_send}
+        self.receivers = {'login': self._transport_login, 'logout': self._transport_logout, 'send': self._transport_send}
 
         # Chat member dictionary in the format: {<member peername>: <member writer>}
         # Also the username is stored in <member writer> value
@@ -56,57 +49,51 @@ class ChatServerManager(BaseServerManager):
 
     async def connection_handle(self, reader, writer):
         try:
+            # Waiting for authentication data from client
             data_json = await self.read(reader)
             if data_json['type'] != 'login':
                 return
-            await self.connection_login(writer, data_json)
+            await self._transport_login(writer, data_json)
 
-            response = {'type': 'send',
-                        'username': data_json['username'],
-                        'content': self.storage.objects
-                        }
-            self.write(writer, response)
+            # Sending chat history
+            chat_history_response = {'type': 'send', 'content': self.storage.objects}
+            self.write(writer, chat_history_response)
 
+            # Listening to incoming messages from the client
             while True:
                 data_json = await self.read(reader)
-                await self.handlers[data_json['type']](writer, data_json)
+                await self.receivers[data_json['type']](writer, data_json)
 
         except (ConnectionError, ChatServerError) as e:
-            await self.connection_logout(writer)
+            await self._transport_logout(writer)
 
-    async def connection_login(self, writer, data_json):
+    async def _transport_login(self, writer, data_json):
         peername = writer.get_extra_info('peername')
-        print('connect:', peername)
+        print(f"[{time.strftime('%x %X')}] connect: {peername} as '{data_json.get('username')}'")
 
         is_occupied = data_json['username'] in [writer.username for writer in self.members.values()]
 
         # Sending a response to the user
-        data_json['status'] = 'fail' if is_occupied else 'ok'
+        data_json['status'] = 'this username is already taken' if is_occupied else 'ok'
         self.write(writer, data_json)
 
         if is_occupied:
-            raise ChatServerError('this username is taken')
+            await self._transport_logout(writer)
         else:
             writer.username = data_json['username']
             self.members[peername] = writer
 
-    async def connection_logout(self, writer, data_json=None):
+    async def _transport_logout(self, writer, data_json=None):
         peername = writer.get_extra_info('peername')
-        print('disconnect:', peername)
-
-        if data_json is not None:
-            data_json['status'] = 'ok'
-            self.write(writer, data_json)
+        print(f"[{time.strftime('%x %X')}] disconnect: {peername}", end=' forcibly\n' if data_json is None else '\n')
 
         if peername in self.members:
             self.members.pop(peername)
 
-        writer.close()
-        if not writer.is_closing():
-            await writer.wait_closed()
+        writer.transport.pause_reading()
 
     @login_required
-    async def connection_send(self, writer, data_json):
+    async def _transport_send(self, writer, data_json):
         self.storage.push_message(data_json['username'], data_json['content'])
 
         # A new message is sent as a list
