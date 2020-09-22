9984244f276854934fe62bf05aeef086f43c6594
ekrainov135 <ekrainov135@gmail.com>
2020-09-22 13:11:35 +0300

    added handling and generation of exceptions

--- client/client.py
+++ client/client.py
@@ -1,10 +1,16 @@
-import asyncio
 import json
 import socket
+import time
+
+
+CLEARING_STRING = "\033[A                             \033[A"
 
 
 class BaseClient:
-    handlers = {}
+
+    def __init__(self):
+        self.is_active = False
+        self.handlers = {}
 
     def __del__(self):
         self._connection.close()
@@ -17,48 +23,71 @@ class BaseClient:
             try:
                 data += self._connection.recv(1024)
             except socket.error as e:
-                raise ChatClientError("error reading data from socket", e)
+                raise ChatClientError("unknown server error", e)
 
-        return data.decode('utf-8')
+        return json.loads(data.decode())
 
-    def write(self, data):
+    def write(self, data_json):
         try:
-            self._connection.send(data.encode("utf8"))
+            self._connection.send(json.dumps(data_json).encode()+b'\n')
         except socket.error as e:
-            raise ChatClientError("error reading data from socket", e)
+            raise ChatClientError("unknown server error", e)
 
-    def connect(self, host='127.0.0.1', port=7070, timeout=None):
+    def connect(self, host, port, timeout=None):
         try:
             self._connection = socket.create_connection((host, int(port)), timeout)
+            self.is_active = True
         except socket.error as e:
-            raise ChatClientError("cannot create connection", e)
+            raise ChatClientError("unknown server error", e)
+
+    def disconnect(self):
+        self.is_active = False
+        self._connection.close()
 
     def echo_server(self):
-        while True:
-            response = json.loads(self.read())
-            self.handlers[response['type']](response)
+        while self.is_active:
+            try:
+                response = self.read()
+                if response['type'] in self.handlers:
+                    self.handlers[response['type']](response)
+            except (ConnectionError, ChatClientError):
+                break
 
 
 class ChatClient(BaseClient):
 
     def __init__(self):
+        super().__init__()
         self.handlers = {'send': self._send}
 
     def _send(self, data_json):
         for message_object in data_json['content']:
-            print(f"({message_object['timestamp']}) {message_object['member']}: {message_object['message']}")
+            print(CLEARING_STRING)
+            print(f"({message_object['timestamp']}) {message_object['member']}: {message_object['message']}\n")
 
-    def authorize(self, username):
+    def login(self, username, server=('127.0.0.1', 7070)):
+        if not self.is_active:
+            self.connect(*server)
+
         request = {'type': 'login', 'username': username}
-        self.write(json.dumps(request))
-        response = json.loads(self.read())
-        print(response)
+        self.write(request)
+        response = self.read()
+        if response['status'] != 'ok':
+            raise ChatClientError(response['status'])
         self.username = username
-        return response['status'] == 'ok'
+        
+    def logout(self):
+        request = {'type': 'logout', 'username': self.username}
+        self.write(request)
+
+        # Wait the server closes the connection itself
+        # Connection is broken forced if the server did not time to do
+        time.sleep(0.2)
+        self.disconnect()
 
     def send(self, message):
         request = {'type': 'send', 'username': self.username, 'content': message}
-        self.write(json.dumps(request))
+        self.write(request)
 
 
 class ChatClientError(Exception):
