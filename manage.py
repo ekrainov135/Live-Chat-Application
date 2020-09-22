3c6994761aa90af8eaf4fd788f43a77dd770e5b8
ekrainov135 <ekrainov135@gmail.com>
2020-09-22 13:04:59 +0300

    logic for starting the event loop and starting the server moved to manage.py

--- manage.py
+++ manage.py
@@ -1,9 +1,32 @@
+import asyncio
+import sys
+
 from core.server import ChatServerManager
 
 
+def run_chat_server(host='127.0.0.1', port=7070):
+    chat_server_manager = ChatServerManager()
+    loop = asyncio.get_event_loop()
+    coro = asyncio.start_server(chat_server_manager.connection_handle, host, int(port), loop=loop)
+    server = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
+
+
 def main():
-    s = ChatServerManager()
-    s.start('127.0.0.1', 7070)
+    COMMAND_DEFAULT = 'run_chat_server'
+    console_command = COMMAND_DEFAULT if len(sys.argv) == 1 else sys.argv[1]
+    if console_command == 'run_chat_server':
+        run_chat_server('127.0.0.1', 7070)
+    else:
+        pass
 
 
 if __name__ == '__main__':
